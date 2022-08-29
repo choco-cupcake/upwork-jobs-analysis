f2621d03324f0781ae9a9f5135f89db8b2154af2
choco-cupcake <98981813+choco-cupcake@users.noreply.github.com>
2022-08-29 18:54:54 +0200

    Module finished

--- JobsScraper.js
+++ JobsScraper.js
@@ -1,9 +1,13 @@
 const puppeteer = require('puppeteer-extra');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config()
 
+let mongoDB
+let mongo_dbName = 'UpworkJobsAnalysis'
 
-const keywords = ['solidity']
+const keywords = ['solidity', 'ethereum', 'blockchain', 'evm', 'smart contract', 'nft', 'defi', 'web3']
 const nextButtonXP = "//span[contains(@class,'up-pagination-label') and contains(text(), 'Next')]/.."
+const nextButtonDisabledXP = "//button[@disabled]/span[contains(@class,'up-pagination-label') and contains(text(), 'Next')]"
 
 let _keyword = null
 let page
@@ -11,18 +15,23 @@ let page
 main()
 
 async function main(){
+    await setupMongoDB()
     await puppeteerBoot()
     await setRequestInterceptor()
-    // mongodb setup
 
     for(let keyword of keywords){
         _keyword = keyword
+        await createDbCollection()
         await scrapeKeyword()
     }
 }
 
+async function createDbCollection(){
+    try {
+        await mongoDB.db(mongo_dbName).createCollection(_keyword)
+    } catch(e) {}
+}
 async function scrapeKeyword(){
-    // todo decide filters vs attributes to analyze
     let url = "https://www.upwork.com/nx/jobs/search/?q=" + _keyword + "&sort=recency"
     await page.goto(url)
     await scrapePage()
@@ -30,13 +39,18 @@ async function scrapeKeyword(){
 }
 
 async function scrapePage(){
-    // if next button disabled return 
-    // else wait load + click next + await scrapePage()
+    await page.waitForXPath(nextButtonXP, {timeout: 10000});
+    await sleep(5000)
+    let nextBtnElem = (await page.$x(nextButtonXP))[0];
+    let nextBtnDisabled = await page.$x(nextButtonDisabledXP);
+    if(nextBtnDisabled.length) 
+        return
+    await nextBtnElem.click()
+    await scrapePage()
 }
 
-function parseJobs(jobs){
-    // clean
-    // update to database into keyword folder
+async function parseJobs(jobs){
+    await mongoDB.db(mongo_dbName).collection(_keyword).insertMany(jobs)
 }
 
 async function puppeteerBoot() {
@@ -58,6 +72,17 @@ async function setRequestInterceptor(){
 		if(request.method() == "GET" && (request.url().includes("https://www.upwork.com/ab/jobs/search/url") || request.url().includes("https://www.upwork.com/search/jobs/url"))){
             let responseRaw = await response.text()
             let jobs = JSON.parse(responseRaw).searchResults.jobs
+            await parseJobs(jobs)
         }
     })
+}
+
+async function setupMongoDB(){
+    const uri = "mongodb+srv://admin:" + process.env.MONGODB_PWD + "@cluster0.9pbl5q2.mongodb.net/?retryWrites=true&w=majority";
+    mongoDB = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    await mongoDB.connect();
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
